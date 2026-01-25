7585da5db6200c064a1356d65fe39efdb0bb9c7c
Paul Dale <ldpercy@gmail.com>
2026-01-25 14:24:58 +1100

    Colour scheme updates from common

--- app/view-svgdoc.js
+++ app/view-svgdoc.js
@@ -161,15 +161,7 @@ class SVGDoc {
 		}
 
 
-		if (ui.colourScheme === 'light')
-		{
-			document.body.classList.remove('dark');
-			document.body.classList.add('light');
-		}
-		else {
-			document.body.classList.remove('light');
-			document.body.classList.add('dark');
-		}
+		wormfarmApp.setColourScheme(ui.colourScheme);
 
 		element.cartesianGrid.style.setProperty('opacity', ui.cartesianOpacity);
 
